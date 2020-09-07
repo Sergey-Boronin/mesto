7fa3a639422104434377b9bd4522eb5ea04a078b
Sergey Boronin <bo-ronin@yandex.ru>
2020-09-07 14:53:52 +0300

    v4

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,15 @@ let jobInput = document.querySelector('.popup__input_type_job');
 let popupSaveButton = document.querySelector('.popup__save');
 let popupForm = document.querySelector('.popup__form')
 
-function popupToggle () {
-  popup.classList.toggle('popup_opened')
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+function popupToggle() {
+  popup.classList.toggle("popup_opened");
+  if (popup.classList.contains("popup_opened")) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  } else {
+    nameInput.value = "";
+    jobInput.value = "";
+  }
 }
 
 function formSubmitHandler (evt) {
