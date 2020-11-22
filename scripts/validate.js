f4e0f5e82198b8fc6d68da64123a047fc56676f4
Sergey Boronin <bo-ronin@yandex.ru>
2020-11-22 17:33:19 +0300

    refactor

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,16 +1,20 @@
 const formEdit = document.querySelector('.popup-edit__form');
-const editNameInput = formEdit.querySelector('.popup__input_type_name');
+const editInputsList = formEdit.querySelectorAll('.popup__input');
 
 
-editNameInput.addEventListener('input', (evt) => {
-  const input = editNameInput;
-  const error = formEdit.querySelector(`#${input.id}-error`);
-  error.textContent = input.validationMessage;
 
-  if(!input.validity.valid){
-    input.classList.add('popup__input_state_invalid')
-  } else {
-    input.classList.remove('popup__input_state_invalid')
-  }
-})
+editInputsList.forEach((input) => {
+  input.addEventListener('input', () => {
+    const error = formEdit.querySelector(`#${input.id}-error`);
+    error.textContent = input.validationMessage;
+
+    if(!input.validity.valid){
+      input.classList.add('popup__input_state_invalid')
+    } else {
+      input.classList.remove('popup__input_state_invalid')
+    }
+  });
+});
+
+
 
