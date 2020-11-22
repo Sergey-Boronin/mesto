75fd2569eeb2227cc81d29c33fa16b5b0aad0e0c
Sergey Boronin <bo-ronin@yandex.ru>
2020-11-22 18:07:06 +0300

    refactor + feat: описать состояние кнопки

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,18 +1,43 @@
 const formEdit = document.querySelector('.popup-edit__form');
 const editInputsList = formEdit.querySelectorAll('.popup__input');
+const editSubmitButton = formEdit.querySelector('.popup__button');
 
 
+
+function showError(form, input) {
+  const error = form.querySelector(`#${input.id}-error`);
+  error.textContent = input.validationMessage;
+  input.classList.add('popup__input_state_invalid')
+}
+function hideError(form, input) {
+  const error = form.querySelector(`#${input.id}-error`);
+  error.textContent = '';
+  input.classList.remove('popup__input_state_invalid')
+}
+
+function checkInputValidity(form, input) {
+  if(!input.validity.valid){
+    showError(form, input)
+  } else {
+    hideError(form, input)
+  }
+}
+
+
+function setButtonState (button, isActive) {
+  if(isActive){
+    button.classList.remove('popup__button_invalid');
+    button.disabled = false;
+  } else {
+    button.classList.add('popup__button_invalid');
+    button.disabled = true    ;
+  }
+}
 
 editInputsList.forEach((input) => {
   input.addEventListener('input', () => {
-    const error = formEdit.querySelector(`#${input.id}-error`);
-    error.textContent = input.validationMessage;
-
-    if(!input.validity.valid){
-      input.classList.add('popup__input_state_invalid')
-    } else {
-      input.classList.remove('popup__input_state_invalid')
-    }
+    checkInputValidity(formEdit, input);
+    setButtonState (editSubmitButton, formEdit.checkValidity())
   });
 });
 
