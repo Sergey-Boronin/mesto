1bcb8a4f96cf5f86ec33f697dac397f89881a799
Sergey Boronin <bo-ronin@yandex.ru>
2020-12-09 02:34:38 +0300

    refactor: вынести массив с константами для валидатора в отдельный файл

---
+++ scripts/constants.js
@@ -0,0 +1,8 @@
+export const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  buttonInvalidClass: 'popup__button_invalid',
+  inputInvalidClass: 'popup__input_state_invalid',
+  formError: '.popup__input-error',
+};
